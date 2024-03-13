3ae4c06aedad0ec43c3f2ce79275f090e5e50a67
Jun Xiu Chan <51901925+missingems@users.noreply.github.com>
2024-03-13 19:08:38 +0800

    Setup search table view controller and view model

---
+++ Oracle/Search/Model/SearchTableViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  SearchTableViewModel.swift
+//  Oracle
+//
+//  Created by Jun on 13/3/24.
+//
+
+import Foundation
+
+final class SearchTableViewModel {
+  
+}
---
+++ Oracle/Search/Views/SearchTableViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchTableViewController.swift
+//  Oracle
+//
+//  Created by Jun on 13/3/24.
+//
+
+import UIKit
+
+final class SearchTableViewController: UITableViewController {
+  init() {
+    super.init(style: .insetGrouped)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
