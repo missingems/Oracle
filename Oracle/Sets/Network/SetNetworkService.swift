ce0a91774ceb68479ae72925e94255706aeebe18
Jun Xiu Chan <51901925+missingems@users.noreply.github.com>
2024-03-20 21:18:06 +0800

    Update SetNetworkService.swift

--- Oracle/Sets/Network/SetNetworkService.swift
+++ Oracle/Sets/Network/SetNetworkService.swift
@@ -1,27 +1,21 @@
-//
-//  SetsTableViewNetworkService.swift
-//  Oracle
-//
-//  Created by Jun on 13/3/24.
-//
-
 import Foundation
 import ScryfallKit
 
-protocol SetNetworkService {
-  func fetchSets(completion: @escaping (Result<[any GameSet], Error>) -> ())
-  func querySets(query: String, in sets: [any GameSet], completion: @escaping (Result<[any GameSet], Error>) -> ())
-  func queryCards(query: String, completion: @escaping (Result<[String], Error>) -> ())
+struct QueryResponse {
+  let sets: [any GameSet]
+  let cardNames: [String]
 }
 
-extension ScryfallClient: SetNetworkService {
+final class SetNetworkService {
+  private var client = ScryfallClient()
+  
   func fetchSets(completion: @escaping (Result<[any GameSet], any Error>) -> ()) {
-    getSets { result in
+    client.getSets { result in
       switch result {
       case let .success(value):
         DispatchQueue.global().async {
-          var data = value.data.filter { !$0.digital && $0.numberOfCards != 0 }
-          var sections: [[MTGSet]] = data.filter { $0.parentSetCode == nil }.map { [$0]}
+          var data = value.data.filter { $0.digital == false && $0.numberOfCards != 0 }
+          var sections = data.filter { $0.parentSetCode == nil }.map { [$0] }
           
           for (index, section) in sections.enumerated() {
             sections[index] = section + data.filter {
@@ -30,7 +24,6 @@ extension ScryfallClient: SetNetworkService {
           }
           
           data = sections.flatMap { $0 }
-          
           completion(.success(data))
         }
         
@@ -40,7 +33,54 @@ extension ScryfallClient: SetNetworkService {
     }
   }
   
-  func querySets(query: String, in sets: [any GameSet], completion: @escaping (Result<[any GameSet], Error>) -> ()) {
+  func query(
+    _ query: String,
+    sets: [any GameSet] = [],
+    completion: @escaping (Result<QueryResponse, Error>) -> ()
+  ) {
+    var gameSets: [any GameSet] = [] {
+      didSet {
+        completion(.success(QueryResponse(sets: gameSets, cardNames: cardNames)))
+      }
+    }
+    
+    var cardNames: [String] = [] {
+      didSet {
+        completion(.success(QueryResponse(sets: gameSets, cardNames: cardNames)))
+      }
+    }
+    
+    querySets(
+      query: query,
+      in: sets
+    ) { result in
+      switch result {
+      case let .success(value):
+        gameSets = value
+        
+      case let .failure(error):
+        completion(.failure(error))
+      }
+    }
+    
+    queryCards(
+      query: query
+    ) { result in
+      switch result {
+      case let .success(value):
+        cardNames = value
+        
+      case let .failure(error):
+        completion(.failure(error))
+      }
+    }
+  }
+  
+  func querySets(
+    query: String,
+    in sets: [any GameSet],
+    completion: @escaping (Result<[any GameSet], Error>) -> ()
+  ) {
     func distanceFromTarget(_ string: String, target: Character) -> Int {
       guard let firstCharacter = string.uppercased().first else { return Int.max }
       return abs(Int(firstCharacter.asciiValue ?? 0) - Int(target.asciiValue ?? 0))
@@ -48,11 +88,6 @@ extension ScryfallClient: SetNetworkService {
     
     DispatchQueue.global().async { [weak self] in
       guard self != nil else {
-        return
-      }
-      
-      guard query.isEmpty == false else {
-        completion(.failure(SetNetworkServiceError.noQuery))
         return
       }
       
@@ -69,7 +104,7 @@ extension ScryfallClient: SetNetworkService {
   }
   
   func queryCards(query: String, completion: @escaping (Result<[String], Error>) -> ()) {
-    getCardNameAutocomplete(query: query) { [weak self] result in
+    client.getCardNameAutocomplete(query: query) { [weak self] result in
       guard self != nil else {
         return
       }
