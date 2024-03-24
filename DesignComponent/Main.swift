c11670cd295099a76a8ce5bfcb1f1ca0e1e3d591
Jun Xiu Chan <51901925+missingems@users.noreply.github.com>
2024-03-24 18:36:55 +0800

    Removed anchorage, kingfisher, scryfall

--- DesignComponent/Main.swift
+++ DesignComponent/Main.swift
@@ -1,6 +1,20 @@
 import SDWebImageSVGNativeCoder
 
+#if os(iOS)
+import UIKit
+#elseif os(macOS)
+import AppKit
+#endif
+
 public struct Main {
+  #if os(iOS)
+    static let nativeScale = UIScreen.main.nativeScale
+  #elseif os(macOS)
+    static let nativeScale = NSScreen.main?.backingScaleFactor ?? 1.0
+  #else
+    static let nativeScale = 3.0
+  #endif
+    
   public static func setupSVGCoder() {
     let SVGCoder = SDImageSVGNativeCoder.shared
     SDImageCodersManager.shared.addCoder(SVGCoder)
