e7e842fe3b47f863f3237509cf8ef7725888d23e
Jun Xiu Chan <51901925+missingems@users.noreply.github.com>
2024-04-01 16:36:48 +0800

    Update QueryResultView.swift

--- Set/Query/QueryResultView.swift
+++ Set/Query/QueryResultView.swift
@@ -8,34 +8,38 @@ struct QueryResultView: View {
   var store: StoreOf<QueryFeature>
   
   var body: some View {
-    content.toolbar {
-      ToolbarItem(placement: .navigation) {
-        Menu {
-          Button("List", systemImage: "list.bullet") {
-            store.send(.viewStateChanged(.list))
+    content
+      .toolbar {
+        ToolbarItem(placement: .navigation) {
+          Menu {
+            Button("List", systemImage: "list.bullet") {
+              store.send(.viewStateChanged(.list))
+            }
+            
+            Button("Grid", systemImage: "square.grid.2x2.fill") {
+              store.send(.viewStateChanged(.grid))
+            }
+          } label: {
+            Image(systemName: "ellipsis.circle")
           }
-          
-          Button("Grid", systemImage: "square.grid.2x2.fill") {
-            store.send(.viewStateChanged(.grid))
-          }
-        } label: {
-          Image(systemName: "ellipsis.circle")
         }
       }
-    }
-    .navigationBarTitleDisplayMode(.inline)
-    .navigationTitle(store.title)
-    .background { Color(.secondarySystemBackground).ignoresSafeArea() }
-    .onAppear { store.send(.viewAppeared) }
+      .navigationBarTitleDisplayMode(.inline)
+      .navigationTitle(store.title)
+      .background { Color(.secondarySystemBackground).ignoresSafeArea() }
+      .onAppear { store.send(.viewAppeared) }
   }
-  
   
   @ViewBuilder
   private var content: some View {
-    if store.viewState == .grid {
-      QueryResultView.GridView(store: store)
-    } else {
-      QueryResultView.ListView(store: store)
+    VStack {
+      
+      
+      if store.viewState == .grid {
+        QueryResultView.GridView(store: store)
+      } else {
+        QueryResultView.ListView(store: store)
+      }
     }
   }
 }
