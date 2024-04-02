27398b22ded86ea62090d940883ead762df7a3c2
Jun Xiu Chan <51901925+missingems@users.noreply.github.com>
2024-04-03 04:15:10 +0800

    Update QueryResultGridView.swift

--- Set/Query/QueryResultGridView.swift
+++ Set/Query/QueryResultGridView.swift
@@ -12,27 +12,19 @@ extension QueryResultView {
     ]
     
     var body: some View {
-      GeometryReader { proxy in
-        if proxy.size != .zero {
-          let itemWidth = (proxy.size.width - 15) / CGFloat(columns.count)
-          let itemHeight = itemWidth * 1.3928
-          
-          ScrollView {
-            LazyVGrid(columns: columns, spacing: 8) {
-              ForEach(store.displayingCards.indices, id: \.self) { index in
-                let card = store.displayingCards[index]
-                
-                NavigationLink(state: Feature.Path.State.showCard(CardFeature.State(card: card))) {
-                  AmbientWebImage(url: card.getImageURL(type: .normal), placeholderName: "mtgBack")
-                    .frame(width: itemWidth, height: itemHeight)
-                    .redacted(reason: store.redactionReason)
-                    .onAppear { store.send(.loadMoreIfNeeded(index)) }
-                }
-              }
+      ScrollView {
+        LazyVGrid(columns: columns, spacing: 8) {
+          ForEach(store.displayingCards.indices, id: \.self) { index in
+            let card = store.displayingCards[index]
+            
+            NavigationLink(state: Feature.Path.State.showCard(CardFeature.State(card: card))) {
+              AmbientWebImage(url: card.getImageURL(type: .normal), placeholderName: "mtgBack")
+                .redacted(reason: store.redactionReason)
             }
-            .padding(EdgeInsets(top: 5, leading: 5, bottom: 13, trailing: 5))
+            .onAppear { store.send(.loadMoreIfNeeded(index)) }
           }
         }
+        .padding(EdgeInsets(top: 5, leading: 5, bottom: 13, trailing: 5))
       }
     }
   }
