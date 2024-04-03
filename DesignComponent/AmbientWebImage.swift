1ebec2a3267ce9730f8a5eaf38e51647c2bd01b9
Jun Xiu Chan <51901925+missingems@users.noreply.github.com>
2024-04-03 13:14:48 +0800

    Update AmbientWebImage.swift

--- DesignComponent/AmbientWebImage.swift
+++ DesignComponent/AmbientWebImage.swift
@@ -15,13 +15,15 @@ public struct AmbientWebImage: View {
   public var body: some View {
     ZStack {
       WebImage(url: url) { image in
-        image.resizable().blur(radius: 20, opaque: false)
+        image.resizable().blur(radius: 44, opaque: false)
       } placeholder: {
         Image(placeholderName, bundle: .main).resizable().clipShape(.rect(cornerRadii: .init(topLeading: cornerRadius, bottomLeading: cornerRadius, bottomTrailing: cornerRadius, topTrailing: cornerRadius)))
       }
       .scaledToFit()
       .aspectRatio(contentMode: .fit)
       .opacity(0.38)
+      .scaleEffect(CGSize(width: 1.1, height: 1.1))
+      .offset(x: 0, y: 10)
       
       WebImage(url: url) { image in
         image.resizable()
