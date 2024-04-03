e7060cfa2944cfddcde58ca6ca457f4ee5a22a6d
Jun Xiu Chan <51901925+missingems@users.noreply.github.com>
2024-04-03 12:27:56 +0800

    Update QueryResultGridView.swift

--- Set/Query/QueryResultGridView.swift
+++ Set/Query/QueryResultGridView.swift
@@ -7,8 +7,8 @@ extension QueryResultView {
     let store: StoreOf<QueryFeature>
     
     let columns: [GridItem] = [
-      GridItem(.flexible(), spacing: 5),
-      GridItem(.flexible(), spacing: 5),
+      GridItem(.flexible(), spacing: 8),
+      GridItem(.flexible(), spacing: 8),
     ]
     
     var body: some View {
@@ -17,14 +17,24 @@ extension QueryResultView {
           ForEach(store.displayingCards.indices, id: \.self) { index in
             let card = store.displayingCards[index]
             
-            NavigationLink(state: Feature.Path.State.showCard(CardFeature.State(card: card))) {
-              AmbientWebImage(url: card.getImageURL(type: .normal), placeholderName: "mtgBack")
-                .redacted(reason: store.redactionReason)
+            NavigationLink(
+              state: Feature.Path.State.showCard(
+                CardFeature.State(
+                  card: card,
+                  cardSetImageURI: store.state.selectedSet.iconSvgUri
+                )
+              )
+            ) {
+              LazyVStack {
+                AmbientWebImage(url: card.getImageURL(type: .normal), placeholderName: "mtgBack")
+                  .frame(width: 184.5, height: 257.09)
+                  .redacted(reason: store.redactionReason)
+              }
             }
             .onAppear { store.send(.loadMoreIfNeeded(index)) }
           }
         }
-        .padding(EdgeInsets(top: 5, leading: 5, bottom: 13, trailing: 5))
+        .padding(EdgeInsets(top: 5, leading: 8, bottom: 13, trailing: 8))
       }
     }
   }
