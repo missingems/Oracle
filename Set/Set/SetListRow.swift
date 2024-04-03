180ff1024c8d6cf0684da384e9c1cef570536ea2
Jun Xiu Chan <51901925+missingems@users.noreply.github.com>
2024-04-03 11:13:44 +0800

    Update SetListRow.swift

--- Set/Set/SetListRow.swift
+++ Set/Set/SetListRow.swift
@@ -8,28 +8,26 @@ struct SetListRow: View {
   let viewModel: SetListRowViewModel
   
   var body: some View {
-    LazyVStack {
-      HStack(spacing: 11.0) {
-        childIndicatorImage.frame(width: 30, height: 30, alignment: .trailing)
-        iconImage.frame(width: 30, height: 30, alignment: .center)
+    HStack(spacing: 11.0) {
+      childIndicatorImage.frame(width: 30, height: 30)
+      iconImage.frame(width: 30, height: 30, alignment: .center)
+      
+      VStack(alignment: .leading, spacing: 5.0) {
+        titleLabel
         
-        VStack(alignment: .leading, spacing: 5.0) {
-          titleLabel
-          
-          HStack(spacing: 5.0) {
-            setCodeLabel
-            numberOfCardsLabel
-          }
+        HStack(spacing: 5.0) {
+          setCodeLabel
+          numberOfCardsLabel
         }
-        
-        Spacer()
-        disclosureIndicator
       }
-      .padding(EdgeInsets(top: 8, leading: 11, bottom: 11, trailing: 11))
-      .background { viewModel.shouldSetBackground ? Color.quaternarySystemFill : Color.clear }
-      .clipShape(.buttonBorder)
-      .padding(EdgeInsets(top: 0, leading: 16.0, bottom: 0, trailing: 16.0))
+      
+      Spacer()
+      disclosureIndicator
     }
+    .padding(EdgeInsets(top: 8, leading: 13, bottom: 11, trailing: 13))
+    .background { viewModel.shouldSetBackground ? Color.quaternarySystemFill : Color.clear }
+    .clipShape(.buttonBorder)
+    .padding(EdgeInsets(top: 0, leading: 16.0, bottom: 0, trailing: 16.0))
   }
 }
 
@@ -39,7 +37,8 @@ extension SetListRow {
   private var childIndicatorImage: some View {
     Container(predicate: viewModel.isSetParent) {
       Image(systemName: viewModel.childIndicatorImageName)
-        .foregroundColor(.tertiaryLabel)
+        .tint(.tertiaryLabel)
+        .fontWeight(.medium)
         .imageScale(.small)
     }
   }
@@ -49,20 +48,21 @@ extension SetListRow {
   }
   
   private var titleLabel: some View {
-    Text(viewModel.title).lineLimit(2).multilineTextAlignment(.leading)
+    Text(viewModel.title).multilineTextAlignment(.leading).tint(.primary)
   }
   
   private var setCodeLabel: some View {
-    PillText(viewModel.id).font(.caption).monospaced()
+    PillText(viewModel.id).font(.caption).monospaced().tint(.primary)
   }
   
   private var numberOfCardsLabel: some View {
-    Text(viewModel.numberOfCardsLabel).font(.caption)
+    Text(viewModel.numberOfCardsLabel).font(.caption).tint(.secondary)
   }
   
   private var disclosureIndicator: some View {
     Image(systemName: viewModel.disclosureIndicatorImageName)
-      .foregroundColor(.tertiaryLabel)
+      .tint(.tertiaryLabel)
+      .fontWeight(.medium)
       .imageScale(.small)
   }
 }
