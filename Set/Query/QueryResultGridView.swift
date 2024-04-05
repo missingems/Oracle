f8db818a4fac057b66b2a972c2241512d040d0df
Jun Xiu Chan <51901925+missingems@users.noreply.github.com>
2024-04-05 13:39:03 +0800

    Update QueryResultGridView.swift

--- Set/Query/QueryResultGridView.swift
+++ Set/Query/QueryResultGridView.swift
@@ -12,29 +12,31 @@ extension QueryResultView {
     ]
     
     var body: some View {
-      ScrollView {
-        LazyVGrid(columns: columns, spacing: 8) {
-          ForEach(store.displayingCards.indices, id: \.self) { index in
-            let card = store.displayingCards[index]
-            
-            NavigationLink(
-              state: Feature.Path.State.showCard(
-                CardFeature.State(
-                  card: card,
-                  cardSetImageURI: store.state.selectedSet.iconSvgUri
-                )
-              )
-            ) {
-              LazyVStack {
-                AmbientWebImage(url: card.getImageURL(type: .normal), placeholderName: "mtgBack")
-                  .frame(width: 184.5, height: 257.09)
-                  .redacted(reason: store.redactionReason)
+      GeometryReader { proxy in
+        if proxy.size.width > 0 {
+          ScrollView {
+            LazyVGrid(columns: columns, spacing: 8) {
+              ForEach(store.displayingCards.indices, id: \.self) { index in
+                let card = store.displayingCards[index]
+                
+                NavigationLink(
+                  state: Feature.Path.State.showCard(
+                    CardFeature.State(
+                      card: card,
+                      cardSetImageURI: store.state.selectedSet.iconSvgUri
+                    )
+                  )
+                ) {
+                  AmbientWebImage(url: card.getImageURL(type: .normal))
+                    .frame(height: ((proxy.size.width - 24.0) / 2 * 1.3928).rounded())
+                    .redacted(reason: store.redactionReason)
+                }
+                .onAppear { store.send(.loadMoreIfNeeded(index)) }
               }
             }
-            .onAppear { store.send(.loadMoreIfNeeded(index)) }
+            .padding(EdgeInsets(top: 5, leading: 8, bottom: 13, trailing: 8))
           }
         }
-        .padding(EdgeInsets(top: 5, leading: 8, bottom: 13, trailing: 8))
       }
     }
   }
